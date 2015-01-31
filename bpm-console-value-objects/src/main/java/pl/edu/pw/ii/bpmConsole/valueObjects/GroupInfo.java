ee2e81814d31b0f601c7a16798e20760d282b02c
Bartek Andrzejczak <ba.andrzejczak@gmail.com>
2015-01-31 06:56:03 +0100

    Added validation to GroupInfo

--- bpm-console-value-objects/src/main/java/pl/edu/pw/ii/bpmConsole/valueObjects/GroupInfo.java
+++ bpm-console-value-objects/src/main/java/pl/edu/pw/ii/bpmConsole/valueObjects/GroupInfo.java
@@ -1,11 +1,15 @@
 package pl.edu.pw.ii.bpmConsole.valueObjects;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.validation.constraints.NotNull;
 
 public class GroupInfo {
     @NotNull
+    @Length(min = 2, max = 32)
     public String id;
     @NotNull
+    @Length(min = 2, max = 32)
     public String name;
     @NotNull
     public GroupType type;
